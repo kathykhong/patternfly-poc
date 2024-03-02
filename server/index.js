fa62213362d6746b7123c35d9fba5d87b696d2d6
kathykhong <kaathy@students.cs.ubc.ca>
2024-03-02 12:56:21 -0800

    write to log file and watch for changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const fs = require("fs");
 const path = require("path");
+const readline = require("readline");
 
 const server = http.createServer();
 const io = socketIo(server, {
@@ -12,10 +13,50 @@ const io = socketIo(server, {
 
 const filePath = path.join(__dirname, "logs", "logfile.txt");
 
-let lastKnownSize = 0; // assume file is blank initally for simplicity
+let lastKnownSize = 0;
+let logInterval;
+
+const generateLogEntry = () => {
+  const date = new Date().toISOString().replace("T", " ").split(".")[0];
+  const endpoints = [
+    "/internal/licenseExpiryStatus",
+    "/internal/replicationInfo",
+    "/internal/internalDbWarnings",
+    "/internal/notifications",
+    "/internal/activeAdministrators",
+    "/internal/uiSettings",
+    "/internal/extendSession",
+  ];
+
+  const randomEndpoint =
+    endpoints[Math.floor(Math.random() * endpoints.length)];
+
+  return `${date}| Administrator| Bearer| [0:0:0:0:0:0:0:1]| unknown| GET| ${randomEndpoint}| 200\n`;
+};
+
+let bigBigString = "";
+for (let i = 0; i < 50; i++) {
+  bigBigString += generateLogEntry();
+  if (Math.random() < 0.1) {
+    bigBigString += "\n";
+  }
+}
+
+const writeLogEntries = () => {
+  fs.appendFileSync(filePath, bigBigString);
+};
+
+const startLogInterval = (socket) => {
+  logInterval = setInterval(() => {
+    writeLogEntries();
+  }, 250);
+};
+
+const stopLogInterval = () => {
+  clearInterval(logInterval);
+};
 
 const printNewLogEntries = (socket) => {
-  console.log("print new log entries");
   fs.stat(filePath, (err, stats) => {
     if (err) {
       console.error("Error getting file stats:", err);
@@ -30,7 +71,6 @@ const printNewLogEntries = (socket) => {
       });
 
       readStream.on("data", (newData) => {
-        console.log(newData.toString());
         socket.emit("new-log-entry", newData.toString());
       });
 
@@ -39,12 +79,50 @@ const printNewLogEntries = (socket) => {
   });
 };
 
+const CHUNK_SIZE = 2000000;
+
+// for limit testing
+const sendLinesInBatch = async (filePath, socket) => {
+  console.log("Sending");
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let lines = [];
+  let lineCount = 0;
+
+  for await (const line of rl) {
+    lines.push(line);
+    lineCount++;
+
+    if (lineCount === CHUNK_SIZE) {
+      processLines(lines, socket);
+
+      // reset for the next batch
+      lines = [];
+      lineCount = 0;
+    }
+  }
+
+  if (lines.length > 0) {
+    processLines(lines, socket);
+  }
+};
+
+const processLines = (lines, socket) => {
+  socket.emit("new-log-entry", lines.join("\n"));
+};
+
 io.on("connection", (socket) => {
   console.log("Client connected");
   console.log(socket.id);
 
   socket.on("watch-log", () => {
     console.log("Watching for changes in", filePath);
+    startLogInterval(socket);
+    // sendLinesInBatch("./logs/logfile.txt", socket);
 
     const watcher = fs.watch(filePath, () => {
       printNewLogEntries(socket);
@@ -52,6 +130,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
       console.log("Client disconnected");
+      stopLogInterval();
       watcher.close();
     });
   });
